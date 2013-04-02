c96be43278db0cbf8050d5171e0c1134fe882f46
G. Blake Meike <blake.meike@gmail.com>
2013-04-02 16:46:59 -0700

    checkpoint

--- Yamba/src/com/marakana/android/yamba/StatusActivity.java
+++ Yamba/src/com/marakana/android/yamba/StatusActivity.java
@@ -3,6 +3,7 @@ package com.marakana.android.yamba;
 import com.marakana.android.yamba.clientlib.YambaClient;
 import com.marakana.android.yamba.clientlib.YambaClientException;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.graphics.Color;
@@ -14,8 +15,11 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class StatusActivity extends Activity {
+	private static final String TAG = "STATUS";
+	
 	private static final int STATUS_MAX = 140;
 	private static final int STATUS_WARN = 10;
 	private static final int STATUS_ERROR = 0;
@@ -25,18 +29,39 @@ public class StatusActivity extends Activity {
 				"student",
 				"password",
 				"http://yamba.marakana.com/api");
-		
-		
+
+
 		boolean succeeded = true;
 		try { client.postStatus(status); }
-		catch (YambaClientException e) { succeeded = false; }
-		
+		catch (YambaClientException e) {
+			Log.e(TAG, "Post failed: " + e, e);
+			succeeded = false;
+		}
+
 		return succeeded;
+		
 	}
-	
-	
+
+	public class Poster extends AsyncTask<String, Void, Integer> {
+
+		@Override
+		protected Integer doInBackground(String... params) {
+			return Integer.valueOf(
+					(send(params[0]))
+					? R.string.post_succeeded
+					: R.string.post_failed);
+		}
+
+		protected void onPostExecute(Integer status) {
+			Toast.makeText(StatusActivity.this, status.intValue(), Toast.LENGTH_LONG).show();
+			poster = null;
+		}
+	}
+
+
 	private EditText status;
 	private TextView count;
+	private Poster poster;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -46,11 +71,9 @@ public class StatusActivity extends Activity {
 		((Button) findViewById(R.id.submit)).setOnClickListener(
 				new View.OnClickListener() {
 					@Override
-					public void onClick(View v) {
-						
-					}
+					public void onClick(View v) { postStatus(); }
 				} );
-		
+
 		status = (EditText) findViewById(R.id.status);
 		count = (TextView) findViewById(R.id.count);
 		status.addTextChangedListener(
@@ -68,12 +91,25 @@ public class StatusActivity extends Activity {
 				} );
 	}
 
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
+
 	void setCount() {
 		int n = STATUS_MAX - status.getText().length();
 		count.setText(String.valueOf(n));
 		count.setTextColor(
 				(STATUS_ERROR > n) ? Color.RED : ((STATUS_WARN > n) ? Color.YELLOW : Color.GREEN));
 	} 
+
+	void postStatus() {
+		if (null != poster) { return; }
+		poster = new Poster();
+		String msg = status.getText().toString();
+		status.setText("");
+		poster.execute(msg);
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
